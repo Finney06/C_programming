7e6be254132eb6d97abd16d36b3b8f2cb86b5750
Osajere Finney <osafinney21@gmail.com>
2022-10-21 17:09:26 +0000

    loop

---
+++ loops.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+*	main - Entry
+*	Description: 'loop function'
+*	Return: ?
+**/
+int main(void)
+{
+	int i;
+	for (i = 0; i < 10; i += 1)
+{
+	printf("Loop %d \n", i);
+}
+	printf(" Finally i = %d \n", i);
+}
